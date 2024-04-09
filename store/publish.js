5e5bab84e0520c84e9ea574554b8ee376aa71bec
Utumporn Naikorn <utumpornnaikorn@MacBook-Air-khxng-Utumporn.local>
2024-04-09 15:20:57 +0700

    [fixed]: generate & download qrcode

--- store/publish.js
+++ store/publish.js
@@ -1,5 +1,6 @@
 import fileSaver from 'file-saver'
 import publishApi from '~/APIs/publish'
+import QRCode from 'qrcode';
 
 const baseClientLink = 'https://liff.line.me/1656257347-RrNWv1p3?entityId='
 const LINK = 'https://คิวพร้อม.com?e='
@@ -17,14 +18,41 @@ export const mutations = {
 }
 
 export const actions = {
+  async generateQrCode({ rootState, dispatch }) {
+    const entityId = rootState.user.entityId
+    const url = `https://xn--42c6cjhs2b6b5k.com/?e=` + entityId
+    // const url = `https://คิวพร้อม.com?e=` + entityId
+
+    try {
+      const qrcodeUrl = await QRCode.toDataURL(url, {
+        width: 1000,
+        margin: 1,
+      });
+
+      return qrcodeUrl
+    } catch(err) {
+      console.log(err)
+    }
+  },
   async downloadQrImage({ rootState, dispatch }) {
     try {
       dispatch('appState/toggleIsLoading', null, { root: true })
-      const downloadData = await publishApi()._getQrLink(rootState.user.entityId)
-      const downloadLink = downloadData.data
-      if (downloadLink) {
-        fileSaver.saveAs(downloadLink, 'QR')
-      }
+      const entityId = rootState.user.entityId
+      const qrcodeURL = await dispatch('generateQrCode')
+
+      const a = document.createElement('a');
+      a.href = qrcodeURL;
+      a.download = `QRCode-${entityId}.png`; // Name of the downloaded file
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+
+      // -- old and generate fail ---
+      // const downloadData = await publishApi()._getQrLink(rootState.user.entityId)
+      // const downloadLink = downloadData.data
+      // if (downloadLink) {
+      //   fileSaver.saveAs(downloadLink, 'QR')
+      // }
     } catch (error) {
       dispatch(
         'appState/toggleNotification',
